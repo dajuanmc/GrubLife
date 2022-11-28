6ce1c2479630b7ab1adc2c24e5c81d54468b0433
fiopwk <flopwiki@gmail.com>
2022-11-27 22:32:57 -0500

    update classpath attributes for junit jupiter

---
+++ app/src/test/java/grubuser.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GrubUser {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+		System.out.println("Failed Test Suite");
+	}
+
+}
